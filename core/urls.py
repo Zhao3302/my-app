85466c71b7b742394ef2489ec09c6afe5a0b3838
Артур Мымриков <Arthur@example.com>
2026-02-15 17:09:27 +0200

    Add redirect to home page and fix imports

--- core/urls.py
+++ core/urls.py
@@ -1,19 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from devices import views # Импортируем твои новые функции из views.py
+from django.views.generic import RedirectView
+from devices import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('devices.urls')),
-    path('', RedirectView.as_view(url='/api/devices/', permanent=True)),I
     
-    # Пути для Лабораторной №6
+    # Головна сторінка: тепер вона автоматично перенаправляє на твій API
+    path('', RedirectView.as_view(url='/api/devices/', permanent=True)),
+    
+    # Шляхи для Лабораторної №6 (Автентифікація)
     path('register/', views.register_view, name='register'),
     path('login/', views.login_with_2fa_view, name='login'),
     path('verify-2fa/', views.verify_2fa_view, name='verify_2fa'),
-    path('secure/', views.secure_page, name='secure'),     # Защищенная страница
+    path('secure/', views.secure_page, name='secure'), 
     
-    # Выход из системы
+    # Вихід із системи
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
